961559881ecfefb7f941441aabdb25e83cf03714
William Deeken <wdeeken7@yahoo.com>
2025-07-07 18:36:06 -0500

    Initial code commit

---
+++ water-level-alarm/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Device.Gpio;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+namespace LoraArduCAMHostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get configuration.
+            IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+            
+            Settings? settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+
+            int sensorPin = settings != null ? settings.WaterSensorGPIOPinNumber : 262;
+            Console.WriteLine($"Pin: {sensorPin}");
+            GpioController controller = new GpioController();
+            controller.OpenPin(sensorPin, PinMode.InputPullDown);
+            while (true)
+            {
+                PinValue buttonState = controller.Read(sensorPin);
+
+                if (buttonState == PinValue.Low)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    Console.WriteLine("1");
+                }
+
+                Thread.Sleep(100); 
+            }
+        }
+
+    }
+}
---
+++ water-level-alarm/Settings.cs
@@ -0,0 +1,32 @@
+public class Settings
+{
+    public int WaterSensorGPIOPinNumber { get; set; }
+    public List<Email> Emails { get; set; }
+    public List<PhoneNumber> PhoneNumbers { get; set; }
+    public EmailConfiguration EmailConfiguration { get; set; }
+    public SMSConfiguration SMSConfiguration { get; set; }
+    public EmailMessageConfiguration EmailMessageConfiguration { get; set; }
+    public SMSMessageConfiguration SMSMessageConfiguration { get; set; }
+}
+public class EmailConfiguration {
+
+}
+public class EmailMessageConfiguration
+{
+
+}
+public class SMSMessageConfiguration
+{
+    
+}
+public class SMSConfiguration {
+
+}
+public class Email {
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
+public class PhoneNumber {
+    public string Name { get; set; }
+    public string Number { get; set; }
+}
